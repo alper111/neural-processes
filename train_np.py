f9eafb166bbb9aa0500061c0fbcc432486ff0e85
Alper Ahmetoğlu <ahmetoglu.alper@gmail.com>
2019-09-03 23:37:18 +0300

    update training script

--- train_np.py
+++ train_np.py
@@ -13,7 +13,7 @@ IN_DIM = 2
 HIDDEN_DIM = 128
 QUERY_DIM = 1
 OUTPUT_DIM = 2
-NUM_ITER = 50000
+NUM_ITER = 1000
 
 model = models.CNP(
     in_dim=IN_DIM,
@@ -48,7 +48,7 @@ optimizer = torch.optim.Adam(
 # x = np.linspace(-5., 5.).reshape(-1, 1)
 # mu_p = np.zeros(x.shape[0])
 # cov_p = models.kernel(x, x)
-X = np.load("egg_demonstrations.npy")
+X = np.load("data/egg_demonstrations.npy")
 L = X.shape[1]
 
 
@@ -57,9 +57,9 @@ for i in range(NUM_ITER):
     optimizer.zero_grad()
     # sample = np.random.multivariate_normal(mu_p, cov_p, 1)
     sample = X[np.random.randint(0, X.shape[0])]
-    x_t = torch.linspace(0., 1., L, device=device).view(1, -1)
-    y_t = torch.tensor(sample, dtype=torch.float, device=device).view(1, -1)
-    xy_t = torch.cat([x_t, y_t], dim=0).t()
+    x_t = torch.linspace(0., 1., L, device=device).view(L, -1)
+    y_t = torch.tensor(sample, dtype=torch.float, device=device).view(L, -1)
+    xy_t = torch.cat([x_t, y_t], dim=1)
 
     R = torch.randperm(L)
     num_of_context = torch.randint(1, 5, (1,))
@@ -69,12 +69,15 @@ for i in range(NUM_ITER):
     # predict both context and target. they say it's better
     target_index = R[:(num_of_context+num_of_target)]
 
-    mu, log_std = model(xy_t, context_index, target_index)
+    out = model(xy_t[context_index], x_t[target_index])
+    mu = out[:, 0]
+    log_std = out[:, 1]
+
     # instead of predicting \sigma, predict its log for stability. cuz non-positive \sigma is pointless.
     std = 0.1 + 0.9 * torch.nn.functional.softplus(log_std)
     dists = torch.distributions.Normal(mu, std)
     # calculate loss
-    loss = -dists.log_prob(xy_t[target_index, 1]).mean()
+    loss = -dists.log_prob(y_t[target_index]).mean()
     loss.backward()
     optimizer.step()
     
@@ -91,20 +94,22 @@ with torch.no_grad():
         # sample = X[np.random.randint(0, X.shape[0])]
         ind = np.random.randint(0, 4)
         sample = utils.sample_gaussian(test_set[ind][0], test_set[ind][1], torch.linspace(-0.5, 0.5, L))
-        x_t = torch.linspace(0., 1., L).view(1, -1)
-        y_t = sample.clone().view(1, -1)
-        xy_t = torch.cat([x_t, y_t], dim=0).t()
+        x_t = torch.linspace(0., 1., L).view(L, -1)
+        y_t = sample.clone().view(L, -1)
+        xy_t = torch.cat([x_t, y_t], dim=1)
         R = torch.randperm(L)
         context_index = [0, 100, -1]
-        mean, log_std = model(xy_t, context_index, torch.arange(L))
+        out = model(xy_t[context_index], x_t)
+        mean = out[:, 0]
+        log_std = out[:, 1]
         std = 0.1 + 0.9 * torch.nn.functional.softplus(log_std)
         
         # plt.plot(xy_t[:, 0].numpy(), xy_t[:, 1].numpy(), c='c')
-        plt.plot(torch.linspace(0., 1., L), y_t.squeeze(0), c="c")
+        plt.plot(np.linspace(0., 1., L), y_t.squeeze(0).numpy(), c="c")
         for i in range(X.shape[0]):
-            plt.plot(torch.linspace(0., 1., L), X[i], c="b")
-        plt.plot(xy_t[:, 0].numpy(), mean.numpy(), c='r')
-        plt.fill_between(xy_t[:, 0].numpy(), mean-std, mean+std, facecolor='r', alpha=0.1)
+            plt.plot(np.linspace(0., 1., L), X[i], c="b")
+        plt.plot(np.linspace(0, 1, L), mean.numpy(), c='r')
+        plt.fill_between(np.linspace(0, 1, L), mean-std, mean+std, facecolor='r', alpha=0.1)
         # plt.scatter(x_t[context_index], y_t[context_index], marker="x", c="r")
         
         # mu_s, cov_s = models.posterior_predictive(x_t.t().numpy(), xy_t[context_index, 0].view(-1,1).numpy() , xy_t[context_index, 1].view(-1,1).numpy(), sigma_f=1.0, sigma_y=0.01)
@@ -116,4 +121,4 @@ with torch.no_grad():
         plt.show()
 
 
-torch.save(model.eval().cpu().state_dict(), "model.pth")
+torch.save(model.eval().cpu().state_dict(), "save/model.pth")
