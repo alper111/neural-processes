042ddfbffb575060f90fcb9f77a8d8c4fac315c0
Alper Ahmetoğlu <ahmetoglu.alper@gmail.com>
2019-09-04 15:34:38 +0300

    add args

--- train_np.py
+++ train_np.py
@@ -1,44 +1,76 @@
+import argparse
+import os
+import time
 import torch
 import models
 import utils
 import matplotlib.pyplot as plt
 import numpy as np
 
+parser = argparse.ArgumentParser(description="train a neural process.")
+parser.add_argument("-in_dim", help="input dimension.", type=int, required=True)
+parser.add_argument("-hidden_dim", help="hidden dimension.", type=int, required=True)
+parser.add_argument("-query_dim", help="query dimension.", type=int, required=True)
+parser.add_argument("-out_dim", help="output dimension.", type=int, required=True)
+parser.add_argument("-en_layer", help="number of encoder layers.", type=int, required=True)
+parser.add_argument("-de_layer", help="number of decoder layers.", type=int, required=True)
+parser.add_argument("-nhead", help="number of attention heads.", type=int)
+parser.add_argument("-iter", help="number of iterations.", type=int, required=True)
+parser.add_argument("-lr", help="learning rate.", type=float, default=1e-3)
+parser.add_argument("-out", help="output path.", type=str, required=True)
+parser.add_argument("-seed", help="seed number", type=float, default=None)
+parser.add_argument("-att", help="whether to use self-attention.", type=int, default=1)
+
+args = parser.parse_args()
+
+ATT = True if args.att == 1 else False
+
 if torch.cuda.is_available():
     device = torch.device("cuda:0")
 else:
     device = torch.device("cpu")
 
-IN_DIM = 2
-HIDDEN_DIM = 128
-QUERY_DIM = 1
-OUTPUT_DIM = 2
-NUM_ITER = 1000
+if args.seed is not None:
+    np.random.seed(seed)
+    torch.manual_seed(seed)
 
-model = models.CNP(
-    in_dim=IN_DIM,
-    hidden_dim=HIDDEN_DIM,
-    query_dim=QUERY_DIM,
-    out_dim=OUTPUT_DIM,
-    en_layer=3,
-    dec_layer=3
-)
+if not os.path.exists(args.out):
+    os.makedirs(args.out)
+os.chdir(args.out)
+arg_dict = vars(args)
+for key in arg_dict.keys():
+    print("%s: %s" % (key, arg_dict[key]))
+    print("%s: %s" % (key, arg_dict[key]), file=open("args.txt", "a"))
+print("date: %s" % time.asctime(time.localtime(time.time())))
+print("date: %s" % time.asctime(time.localtime(time.time())), file=(open("args.txt", "a")))
 
-# model = models.ANP(
-#     in_dim=IN_DIM,
-#     hidden_dim=HIDDEN_DIM,
-#     query_dim=QUERY_DIM,
-#     out_dim=OUTPUT_DIM,
-#     en_layer=3,
-#     dec_layer=3,
-#     nhead=8
-# )
+if args.att:
+    model = models.ANP(
+    in_dim=args.in_dim,
+    hidden_dim=args.hidden_dim,
+    query_dim=args.query_dim,
+    out_dim=args.out_dim,
+    en_layer=args.en_layer,
+    dec_layer=args.de_layer,
+    nhead=args.nhead
+    )
+else:
+    model = models.CNP(
+        in_dim=args.in_dim,
+        hidden_dim=args.hidden_dim,
+        query_dim=args.query_dim,
+        out_dim=args.out_dim,
+        en_layer=args.en_layer,
+        dec_layer=args.de_layer
+    )
+
+
 
 model.to(device)
 print(model)
 
 optimizer = torch.optim.Adam(
-    lr=1e-4,
+    lr=args.lr,
     params=model.parameters(),
     betas=(0.9, 0.999),
     amsgrad=True
@@ -48,12 +80,11 @@ optimizer = torch.optim.Adam(
 # x = np.linspace(-5., 5.).reshape(-1, 1)
 # mu_p = np.zeros(x.shape[0])
 # cov_p = models.kernel(x, x)
-X = np.load("data/egg_demonstrations.npy")
+X = np.load("../../data/egg_demonstrations.npy")
 L = X.shape[1]
 
-
 avg_loss = 0.0
-for i in range(NUM_ITER):
+for i in range(args.iter):
     optimizer.zero_grad()
     # sample = np.random.multivariate_normal(mu_p, cov_p, 1)
     sample = X[np.random.randint(0, X.shape[0])]
@@ -62,7 +93,7 @@ for i in range(NUM_ITER):
     xy_t = torch.cat([x_t, y_t], dim=1)
 
     R = torch.randperm(L)
-    num_of_context = torch.randint(1, 5, (1,))
+    num_of_context = torch.randint(1, 6, (1,))
     # num_of_target = torch.randint(2, 10, (1,))
     num_of_target = 1
     context_index = R[:num_of_context]
@@ -77,7 +108,7 @@ for i in range(NUM_ITER):
     std = 0.1 + 0.9 * torch.nn.functional.softplus(log_std)
     dists = torch.distributions.Normal(mu, std)
     # calculate loss
-    loss = -dists.log_prob(y_t[target_index]).mean()
+    loss = -dists.log_prob(y_t[target_index].squeeze(1)).mean()
     loss.backward()
     optimizer.step()
     
@@ -86,39 +117,4 @@ for i in range(NUM_ITER):
         print("iter: %d, loss: %.4f" % (i+1, avg_loss*1e-3))
         avg_loss = 0.0
 
-with torch.no_grad():
-    model.eval().cpu()
-    test_set = [(0.3, -0.5), (-0.3, 0.5), (0.2, -0.6), (-0.2, 0.6)]
-    for i in range(10):
-        # sample = np.random.multivariate_normal(mu_p, cov_p, 1)
-        # sample = X[np.random.randint(0, X.shape[0])]
-        ind = np.random.randint(0, 4)
-        sample = utils.sample_gaussian(test_set[ind][0], test_set[ind][1], torch.linspace(-0.5, 0.5, L))
-        x_t = torch.linspace(0., 1., L).view(L, -1)
-        y_t = sample.clone().view(L, -1)
-        xy_t = torch.cat([x_t, y_t], dim=1)
-        R = torch.randperm(L)
-        context_index = [0, 100, -1]
-        out = model(xy_t[context_index], x_t)
-        mean = out[:, 0]
-        log_std = out[:, 1]
-        std = 0.1 + 0.9 * torch.nn.functional.softplus(log_std)
-        
-        # plt.plot(xy_t[:, 0].numpy(), xy_t[:, 1].numpy(), c='c')
-        plt.plot(np.linspace(0., 1., L), y_t.squeeze(0).numpy(), c="c")
-        for i in range(X.shape[0]):
-            plt.plot(np.linspace(0., 1., L), X[i], c="b")
-        plt.plot(np.linspace(0, 1, L), mean.numpy(), c='r')
-        plt.fill_between(np.linspace(0, 1, L), mean-std, mean+std, facecolor='r', alpha=0.1)
-        # plt.scatter(x_t[context_index], y_t[context_index], marker="x", c="r")
-        
-        # mu_s, cov_s = models.posterior_predictive(x_t.t().numpy(), xy_t[context_index, 0].view(-1,1).numpy() , xy_t[context_index, 1].view(-1,1).numpy(), sigma_f=1.0, sigma_y=0.01)
-        # mu_s = mu_s.reshape(-1)
-        # cov_s = np.sqrt(np.diag(cov_s))
-        # plt.plot(xy_t[:, 0].numpy(), mu_s, c='b')
-        # plt.fill_between(xy_t[:, 0].numpy(), mu_s-cov_s, mu_s+cov_s, facecolor='b', alpha=0.1)
-        plt.scatter(xy_t[context_index, 0], xy_t[context_index, 1], marker='x', c='g')
-        plt.show()
-
-
-torch.save(model.eval().cpu().state_dict(), "save/model.pth")
+torch.save(model.eval().cpu().state_dict(), "model.ckpt")
